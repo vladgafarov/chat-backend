b981c2dca81d9a6226f519f200840d4bdda374a8
Vlad <vlad.gafarov02@gmail.com>
2022-06-06 22:30:12 +0500

    Enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
    const app = await NestFactory.create(AppModule);
    app.useGlobalPipes(new ValidationPipe());
+   app.enableCors({
+      credentials: true,
+      origin: process.env.FRONTEND_URL,
+   });
    await app.listen(3000);
 }
 bootstrap();
