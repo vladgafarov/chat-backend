4399f691e76a1a3e5e137b317a431a29508bfb9f
Vlad Gafarov <g.vladislav@qvedo.com>
2022-09-19 18:02:54 +0500

    add find users by email

---
+++ src/user/user.controller.ts
@@ -0,0 +1,26 @@
+import { Controller, Get, Param, Query } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+
+@Controller('user')
+export class UserController {
+   constructor(private readonly prismaService: PrismaService) {}
+
+   @Get()
+   async findUsersByEmail(@Query('email') email: string) {
+      const users = await this.prismaService.user.findMany({
+         where: {
+            email: {
+               contains: email,
+            },
+         },
+         select: {
+            id: true,
+            email: true,
+            name: true,
+            avatarUrl: true,
+         },
+      });
+
+      return users;
+   }
+}
